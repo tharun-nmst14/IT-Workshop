d737595f5a747f46ab69b5446c3710ffca9b6953
THARUN AZMEERA <138688779+tharun-nmst14@users.noreply.github.com>
2024-04-04 12:07:21 +0530

    Add files via upload

---
+++ xammp.cmd
@@ -0,0 +1,37 @@
+sudo chmod +x xampp-linux-x64-8.2.4-0-installer.run
+[sudo] password for user:  ///password
+
+ sudo ./xampp-linux-x64-8.2.4-0-installer.run
+      >>>//...opening xampp apk....
+  
+cd /opt/lampp/
+      >>>///..changing directry to xampp folder   
+
+sudo ./manager-linux-x64.run
+[sudo] password for user:  ///password
+^c
+          >>>>///..... Reopens the apk
+
+(base) user@nmst-1402:~$ cd /opt/lampp/htdocs
+(base) user@nmst-1402:/opt/lampp/htdocs$ ls
+  >>>///.. getting into folder....
+
+(base) user@nmst-1402:/opt/lampp/htdocs$ sudo gedit myphp.php
+    >>>///... opens php file editor....
+
+(base) user@nmst-1402:/opt/lampp/htdocs$ ls
+applications.html  dashboard    img        myphp.php  webalizer
+bitnami.css        favicon.ico  index.php  mysite
+(base) user@nmst-1402:/opt/lampp/htdocs$ ls -al myphp.php
+-rw-r--r-- 1 root root 34 Apr  1 16:14 myphp.php
+(base) user@nmst-1402:/opt/lampp/htdocs$ 
+
+    >>>//... checking the php file exists
+
+step - 2 :
+
+>>//... search this in url :
+        http://localhost:3128/dashboard/myphp.php/
+
+
+
